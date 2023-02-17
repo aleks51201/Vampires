8cfb06e53ec4f8844155aedaceeaf711345aa771
KipoGo <vit-tan@mail.ru>
2023-02-17 14:43:34 +0300

    Add

---
+++ Assets/Vampire/Scripts/EnemyObject/EnemyController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Vampire.EnemyObject
+{
+    public class EnemyController
+    {
+        private EnemyView _enemyView;
+
+
+        public EnemyController(EnemyView enemyView)
+        {
+            _enemyView = enemyView;
+        }
+
+
+        internal void Init()
+        {
+        }
+
+        internal void Update()
+        {
+        }
+    }
+}
---
+++ Assets/Vampire/Scripts/EnemyObject/EnemyView.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Vampire.EnemyObject
+{
+    public class EnemyView:MonoBehaviour
+    {
+        private EnemyController _enemyController;
+
+
+        private void Update()
+        {
+            _enemyController.Update();
+        }
+
+        private void Awake()
+        {
+            _enemyController = new(this);
+            _enemyController.Init();
+        }
+
+        private void OnEnable()
+        {
+            
+        }
+
+        private void OnDisable()
+        {
+            
+        }
+    }
+}
