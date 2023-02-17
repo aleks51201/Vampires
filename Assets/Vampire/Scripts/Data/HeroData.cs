976e43b8a438fbc83a2512551209b9e00fcf2c13
KipoGo <vit-tan@mail.ru>
2023-02-17 11:56:05 +0300

    Add
    Add hero data

---
+++ Assets/Vampire/Scripts/Data/HeroData.cs
@@ -0,0 +1,11 @@
+﻿namespace Vampire.Data
+{
+    public struct HeroData
+    {
+        public string Name;
+        public int LvlForOpen;
+        public int Hp;
+        public float MoveSpeed;
+        public float AttackRate;
+    }
+}
