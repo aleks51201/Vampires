0bcea80cc57390a734c7b1a680d1b33f24d1e59d
KipoGo <vit-tan@mail.ru>
2023-02-17 13:59:12 +0300

    Add
    Add enemy data

---
+++ Assets/Vampire/Scripts/Data/EnemyData.cs
@@ -0,0 +1,10 @@
+﻿namespace Vampire.Data
+{
+    public class EnemyData
+    {
+        public string Name;
+        public int Hp;
+        public float MoveSpeed;
+        public int Damage;
+    }
+}
