64617657028c3752d3c86bccf959f657f0931641
KipoGo <vit-tan@mail.ru>
2023-02-17 14:50:22 +0300

    Add

---
+++ Assets/Vampire/Scripts/EnemyObject/Simulations/FollowMoveLogic.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Vampire.EnemyObject.Simulations
+{
+    public class FollowMoveLogic
+    {
+        private NavMeshAgent _navMeshAgent;
+        private Transform _targetTransform;
+
+        public FollowMoveLogic(NavMeshAgent navMeshAgent, Transform targetTransform)
+        {
+            _navMeshAgent = navMeshAgent;
+            _targetTransform = targetTransform;
+        }
+
+
+        public void FollowMove()
+        {
+            _navMeshAgent.SetDestination(_targetTransform.position);
+        }
+    }
+}
