08996c38ec4446160e3d79ff7156f5ad2e8a2c48
KipoGo <vit-tan@mail.ru>
2023-02-17 22:32:51 +0300

    Add

---
+++ Assets/Vampire/Scripts/GunObject/GunController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Vampire.GunObject
+{
+    internal class GunController
+    {
+        private GunView _gunView;
+
+
+        public GunController(GunView gunView)
+        {
+            _gunView = gunView;
+        }
+
+        internal void Init()
+        {
+        }
+
+        internal void Update()
+        {
+        }
+
+    }
+}
---
+++ Assets/Vampire/Scripts/GunObject/GunView.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Vampire.GunObject
+{
+    public class GunView:MonoBehaviour
+    {
+        private GunController _gunController;
+
+
+        private void Update()
+        {
+            _gunController.Update();
+            
+        }
+
+        private void Awake()
+        {
+            _gunController = new(this);
+            _gunController.Init();
+        }
+    }
+}
