5ef05324ad0571ba846af44aca8428e9f0b47a6c
KipoGo <vit-tan@mail.ru>
2023-02-18 18:51:29 +0300

    Add
    Add pause

---
+++ Assets/Vampire/Scripts/PauseObject/PauseManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Vampire.PauseObject
+{
+    public class PauseManager
+    {
+        private static bool _isPause = false;
+
+
+        public static bool IsPause
+        {
+            get
+            {
+                return _isPause;
+            }
+            private set
+            {
+                _isPause = value;
+            }
+        }
+
+
+        public static Action PausedEvent;
+        public static Action UnpausedEvent;
+
+
+        public static void Pause()
+        {
+            if (_isPause)
+            {
+                return;
+            }
+            _isPause = true;
+            PausedEvent?.Invoke();
+        }
+
+        public static void Unpause()
+        {
+            if (!_isPause)
+            {
+                return;
+            }
+            _isPause = false;
+            UnpausedEvent?.Invoke();
+        }
+    }
+}
